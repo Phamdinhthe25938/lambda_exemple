ef15f6eadfa56a6776fc0d621193d54e0bb324e4
phamdinhthe2509@gmail.com <phamdinhthe2509@gmail.com>
2023-06-25 22:56:05 +0700

    add code lambda kinesis

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -eo pipefail
-aws cloudformation deploy --stack-name my-s3-lambda-stack --template-file template.yml
+aws cloudformation package --template-file template.yml --s3-bucket test-bucket-lambda-dev-01 --output-template-file out.yml
+aws cloudformation deploy --template-file out.yml --stack-name lambda-test-trigger --capabilities CAPABILITY_NAMED_IAM
