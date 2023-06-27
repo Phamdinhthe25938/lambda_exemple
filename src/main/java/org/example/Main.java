65776f354e7b91d58594e6699863fad42d8ba537
the.phamdinh <the.phamdinh@vti.com.vn>
2023-06-27 17:09:47 +0700

    put data

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+//package org.example;
+//
+//import com.amazonaws.services.kinesis.model.GetRecordsRequest;
+//import com.amazonaws.services.kinesis.model.GetRecordsResult;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        GetRecordsRequest recordsRequest = new GetRecordsRequest();
+//        recordsRequest.setShardIterator(shardIterator.getShardIterator());
+//        recordsRequest.setLimit(25);
+//
+//        GetRecordsResult recordsResult = kinesis.getRecords(recordsRequest);
+//        while (!recordsResult.getRecords().isEmpty()) {
+//            recordsResult.getRecords().stream()
+//                .map(record -> new String(record.getData().array()))
+//                .forEach(System.out::println);
+//
+//            recordsRequest.setShardIterator(recordsResult.getNextShardIterator());
+//            recordsResult = kinesis.getRecords(recordsRequest);
+//    }
+//}
