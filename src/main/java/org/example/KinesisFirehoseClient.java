650e6ac2739b6e1ebf4ec33b905d2d4dd7c4dbe0
phamdinhthe2509@gmail.com <phamdinhthe2509@gmail.com>
2023-06-27 22:54:14 +0700

    add code lambda kinesis

---
+++ src/main/java/org/example/KinesisFirehoseClient.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehose;
+import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehoseClientBuilder;
+
+public class KinesisFirehoseClient {
+    public static final String AWS_ACCESS_KEY_ID = "aws.accessKeyId";
+    public static final String AWS_SECRET_KEY = "aws.secretKey";
+
+    static {
+        System.setProperty(AWS_ACCESS_KEY_ID, "AKIAYMAHNDIAJZUH3BYL");
+        System.setProperty(AWS_SECRET_KEY, "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");
+    }
+
+    public static AmazonKinesisFirehose getFirehoseClient(){
+        return AmazonKinesisFirehoseClientBuilder
+                .standard()
+                .withRegion(Regions.EU_WEST_2)
+                .build();
+    }
+}
